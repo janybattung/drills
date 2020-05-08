cda2a6d664d4afd6bfc9b0e1c43dbcde797303dc
Jany Battung <janybattung@gmail.com>
2020-05-08 14:08:24 -0700

    add file

---
+++ getLengthOfTwoWords.js
@@ -0,0 +1,6 @@
+function getLengthOfTwoWords(word1, word2) {
+    let wordsLength = word1 + word2;
+    return wordsLength.length;
+  }
+  let output = getLengthOfTwoWords('hello', 'world');
+  console.log(output);

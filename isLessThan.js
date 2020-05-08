59f2df87c7b8c5f6aea48b2a3523a76988b3837d
Jany Battung <janybattung@gmail.com>
2020-05-08 14:21:00 -0700

    add file

---
+++ isLessThan.js
@@ -0,0 +1,9 @@
+function isLessThan(num1, num2) {
+    if (num2 < num1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  let output = isLessThan (10,15);
+  console.log(output);

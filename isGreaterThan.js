8bbc8cf410c018c2396ba3b574033d1f856aaf17
Jany Battung <janybattung@gmail.com>
2020-05-08 14:24:41 -0700

    add file

---
+++ isGreaterThan.js
@@ -0,0 +1,9 @@
+function isGreaterThan(num1, num2) {
+    if (num2 > num1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  let output = isGreaterThan(14,10);
+  console.log(output);

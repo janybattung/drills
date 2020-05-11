f79f391b6e5b18baea1b3de28e685e6a497447f0
Jany Battung <janybattung@gmail.com>
2020-05-10 18:07:08 -0700

    add file

---
+++ isSameLength.js
@@ -0,0 +1,9 @@
+function isSameLength(word1, word2) {
+    if (word1.length === word2.length) {
+      return true;
+    } else {
+      return false;
+    }
+   }
+   let y = isSameLength ('bite', 'bet');
+   console.log (y);

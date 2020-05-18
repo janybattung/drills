c4c3aec15bbc72941be9326f726a0b19d4191b5b
Jany Battung <janybattung@gmail.com>
2020-05-18 14:39:26 -0700

    add file

---
+++ edaaaabit.js
@@ -0,0 +1,12 @@
+function howManyTimes(num) {
+    let result = 'a';
+ result = result.repeat(num);
+
+   return 'ed' + result + 'bit';
+  // console.log(result);
+
+
+}
+
+let num = 4;
+console.log(howManyTimes(num));

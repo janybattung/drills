bb6de0f9e7c2b744bb65b1dc407da7ed3af7df62
Jany Battung <janybattung@gmail.com>
2020-05-19 14:09:50 -0700

    update file

--- minNmax.js
+++ minNmax.js
@@ -2,20 +2,11 @@ function max(numbers) {
     // [4, 6, 8, 5]
     let currentMax = numbers[0];
     for (let i=1; i < numbers.length; i++){
-       if (numbers[i] > currentMax) { //currentMax is the current apple
+       if (numbers[i] > currentMax) { 
          currentMax = numbers[i];
        }
      }
      return currentMax;
    }
-//    function min(numbers) {
-//      let currentMin = numbers [0];
-//      for (let i=0; i < numbers.length; i++){
-//        if (numbers[i] < currentMin) {
-//          currentMin = numbers[i];
-//        }
-//      }
-//      return currentMin;
-//    }
 let numbers = [4,6,8,5];
 console.log(max(numbers));

c24f119c7c7827eb6cb8d843febc8fa8601b43a9
Jany Battung <janybattung@gmail.com>
2020-05-19 14:08:42 -0700

    update file

--- additiveInverse.js
+++ additiveInverse.js
@@ -2,10 +2,20 @@ function additiveInverse(arr) {
     let result=[];
     for (let i=0;i<arr.length;i++){
      
-        arr.push(-arr[i]);
+        result.push(-arr[i]);
     }
-    return arr;
+    return result;
 }
-var n =[3, -9, 11];
-console.log("Matrix before additive inverse["+ n+"]");
-console.log(additiveInverse(n));
+let arr = [3, -9, 11];
+//console.log("Matrix before additive inverse["+ n+"]");
+console.log(additiveInverse(arr));
+// function additiveInverse(arr) {
+// 	let newArray =[];
+// for (i = 0; i < arr.length; i++){
+// 	let obj = arr[i]
+// 	 newArray.push(-(obj))
+// } 
+// 	return newArray
+// }
+// let arr = [3, -9, 11];
+// console.log (additiveInverse(arr));

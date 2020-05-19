4bfcffdadc1f6e7a4b84205f5de09eb5c20af755
Jany Battung <janybattung@gmail.com>
2020-05-19 14:10:32 -0700

    update file

--- summingTheSquares.js
+++ summingTheSquares.js
@@ -2,9 +2,10 @@ function squaresSum(n) {
 	let currentNum = 0;
 	for (i=1; i <= n; i++){
 		//currentNum = currentNum += i*i;
-		currentNum = currentNum += Math.pow (i,2);
+		currentNum = currentNum + Math.pow (i,2);
 	}
-	return currentNum;
+	return currentNum; //1, 5, 9,
+	//
 }
-let n = (6);
+let n = (4);
 console.log(squaresSum(n));

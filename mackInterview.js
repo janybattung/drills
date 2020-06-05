d57e4c85847343a418cc9f9e0b2fb6a932d10117
Jany Battung <janybattung@gmail.com>
2020-06-05 15:50:58 -0700

    add file

---
+++ mackInterview.js
@@ -0,0 +1,20 @@
+// function arrNum(num) {
+//     let sum = 0;
+// for (i=0; i<num.length; i++) {
+//     sum = sum + num[i];
+// } 
+// return sum;
+// }
+// let  = [];
+// console.log(arrNum(num));
+
+function carData(caName) {
+    let describeCar = {
+        make: "Honda",
+        color: "blue",
+        year: 2010
+    }
+    return 
+}
+let output = describeCar(make, color);
+console.log()
